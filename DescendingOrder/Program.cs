bdb500bc8b6b377e8b7a543fa0c7e2878b5aeda4
Support <Support@DESKTOP-16NLN76>
2022-08-05 11:37:26 +0100

    Add project files.

---
+++ DescendingOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+namespace DescendingOrder
+{
+    internal class Program //program to sort an array in decreasing order using iterative way
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = new int[] { 9, 5, 3, 10, 15, 16, 10, 19 }; //declaring and initializing the array
+            int numb;
+            for (int i = 0; i < myNumbers.Length - 1; i++) // traverse 0 to array length
+                for (int j = i + 1; j < myNumbers.Length; j++) // traverse i+1 to array length
+                    if (myNumbers[i] < myNumbers[j]) //compare array element with all next element
+                {
+                    numb = myNumbers[i];
+                    myNumbers[i] = myNumbers[j];
+                    myNumbers[j] = numb;
+                }
+            foreach (int i in myNumbers)    
+            Console.WriteLine(i + " ");
+        }
+    }
+}
